e9644eb9b7bec8a86c6fae3728af03eaf47ffcd3
akshaysripada <akshay.sripada@columbia.edu>
2018-04-06 20:00:54 -0700

    first commit

---
+++ FirstResponder/HouseholdModel.swift
@@ -0,0 +1,9 @@
+//
+//  HouseholdModel.swift
+//  FirstResponder
+//
+//  Created by Akshay Sripada on 4/6/18.
+//  Copyright © 2018 Akshay Tracy Co. All rights reserved.
+//
+
+import Foundation
---
+++ FirstResponder/ViewControllerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ViewControllerTableViewCell.swift
+//  FirstResponder
+//
+//  Created by Akshay Sripada on 4/6/18.
+//  Copyright © 2018 Akshay Tracy Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
